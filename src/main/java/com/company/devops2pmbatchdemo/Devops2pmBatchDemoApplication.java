b7637d73034e80cce6dedec52f3349a14edd05a5
venkat179 <venkatasubbaiah179@gmail.com>
2021-04-30 15:38:37 +0530

    added springboot project

---
+++ src/main/java/com/company/devops2pmbatchdemo/Devops2pmBatchDemoApplication.java
@@ -0,0 +1,13 @@
+package com.company.devops2pmbatchdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Devops2pmBatchDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Devops2pmBatchDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/company/devops2pmbatchdemo/Devops2pmBatchDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.devops2pmbatchdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Devops2pmBatchDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
